d213eb73c5f56ebba54cad6ce4d5ffda359af11b
Afanasiy Muksunov <afonyamuksunov@mail.ru>
2019-12-07 16:56:16 +0900

    First commit

---
+++ Learn5/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn5
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваш возраст");
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                //Console.WriteLine(age >= 18 ? "Взрослый" : "Ошибка ввода или несовершеннолетний");
+                /*if (age > 18)
+                {
+                    Console.WriteLine("Взрослый");
+                }
+                else if(age == 0)
+                {
+                    Console.WriteLine("Новорожденный");
+                }
+                else if (age >= 7 && age < 18)
+                {
+                    Console.WriteLine("Школьник");
+                }
+                else if (age == 18)
+                {
+                    Console.WriteLine("Выпускник(ца)");
+                }
+                else if (age > 0 && age < 7 )
+                {
+                    Console.WriteLine("Ребенок");
+                }*/
+                switch (age)
+                {
+                    case 0:
+                        {
+                            Console.WriteLine("Новорожденный");
+                            break;
+                        }
+                    case 7:
+                        {
+                            Console.WriteLine("Школьник");
+                            break;
+                        }
+                    case 18:
+                        {
+                            Console.WriteLine("Выпускник(ца)");
+                            break;
+                        }
+                    case 20:
+                        {
+                            Console.WriteLine("Взрослый");
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Такого значения нету");
+                            break;
+                        }
+                }
+                Console.WriteLine("Конец выполнения программы");
+            }
+            catch
+            {
+                Console.WriteLine("Неправильное значение. Повторите ввод");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
